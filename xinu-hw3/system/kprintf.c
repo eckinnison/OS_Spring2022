f528b3718193fbc413a2c08fd05e0bf6b216e2d5
eckinnison <60713455+eckinnison@users.noreply.github.com>
2022-02-12 13:34:17 -0600

    testing

--- xinu-hw3/system/kprintf.c
+++ xinu-hw3/system/kprintf.c
@@ -34,15 +34,14 @@ syscall kgetc(void)
     // TODO: First, check the unget buffer for a character.
     //       Otherwise, check UART flags register, and
     //       once the receiver is not empty, get character c.
+    while(i < UNGETMAX){
+        if(ungetArray[1]){
+        }
+    }
+    while (!((regptr->fr) & (PL011_FR_RXFE))) { // fr - flag register
         if (ungetArray[1]) {
         }
-    
-    while (!((regptr->fr) & (PL011_FR_RXFE))) { // fr - flag register
     }
-
-    // also from pl011.h 
-    ungetArray[i] = c;  // dr - data register
-   
 
     return SYSERR; 
 }
