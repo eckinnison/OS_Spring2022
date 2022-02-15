46437ab88ee16de7d96c42484c082e3b8e428dbc
eckinnison <60713455+eckinnison@users.noreply.github.com>
2022-02-15 11:53:56 -0600

    trying to get the formatting better

--- xinu-hw3/main/main.c
+++ xinu-hw3/main/main.c
@@ -258,7 +258,7 @@ process main(void)
 	}
 
 
-	kprintf("Most probable key is %d\r\n", keysecond);
+	kprintf("\r\nMost probable key is %d\r\n", keysecond);
 
 	decoder_ring_printer(textarr, keysecond, index);
 	kprintf("\r\n");
