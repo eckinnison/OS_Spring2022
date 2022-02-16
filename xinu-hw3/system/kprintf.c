f630f6e7edff4c009d7c4497efa069d505558d6f
eckinnison <60713455+eckinnison@users.noreply.github.com>
2022-02-15 20:32:08 -0600

    enters still weird

--- xinu-hw3/system/kprintf.c
+++ xinu-hw3/system/kprintf.c
@@ -48,7 +48,7 @@ syscall kgetc(void) //*****************HELP*************
     
     while ((regptr->fr) & (PL011_FR_RXFE)) {
     }
-    if (c == "/r") {
+    if (c == "/r/n") {
         kprintf("here");
     }
     c = regptr->dr;
