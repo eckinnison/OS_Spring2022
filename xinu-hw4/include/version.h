f29ad760aa984d7e30a98e99d55cc2c73b5673bf
David Santiago <dsantiago6@morbius.mscsnet.mu.edu>
2022-02-17 10:21:57 -0600

    new xinu-hw4

---
+++ xinu-hw4/include/version.h
@@ -0,0 +1,1 @@
+#define VERSION "(Embedded Xinu) (arm-rpi3) #15 (dsantiago6@morbius) Fri Feb 11 22:29:13 CST 2022"
---
+++ xinu-hw4/main/main.c
@@ -0,0 +1,34 @@
+/**
+ * COSC 3250 - Project 3
+ * Explain briefly the functionality of the program.
+ * @author Emma Claire Kinnison David Santiago
+ * Instructor Dr. Brylow
+ * TA-BOT:MAILTO emma.kinnison@marquette.edu david.santiago@marquette.edu
+ */
+/**
+ * @file     main.c
+ */
+/* Embedded Xinu, Copyright (C) 2009, 2022.  All rights reserved. */
+
+#include <xinu.h>
+
+/**
+ * Main process.  You can modify this routine to customize what Embedded Xinu
+ * does when it starts up.
+ */
+process main(void)
+{
+    kprintf("Hello Xinu World!\r\n");
+
+   // kprintf("Enter your codebreaker text:\r\n");
+
+    testcases();
+    // TODO: Put your main program for codebreaker here.
+    //       Replace any calls to printf() with kprintf().
+    //       Replace any calls to getchar() with kgetc().
+
+    while (1)
+        ;
+
+    return 0;
+}
